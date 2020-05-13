e642e42522302bff88bcf8da0172b4c6da04fbed
LauraPerim <laurabatmanp@gmail.com>
2020-05-13 17:26:16 -0300

    alt

--- jogo.py
+++ jogo.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed May 13 16:11:08 2020
-
-@author: laura
+Insper
+Authors: Diogo Nobre de Araujo Cintra, Luís Eduardo Satou Ferreira Pinheiro and Laura Batman Perim
+Graduating in Mechanical Engineering, Mechatronical Engineering and Computer Engineering
+Email: diogonac@al.insper.edu.br
+Email: luisesfp@al.insper.edu.br
+Email: laurabp@al.insper.edu.br
 """
 
+import pygame as pg
+from os import path
+import random
+from os import path
+
+bg = pygame.image.load("C:\Users\laura\Documents\pygame2")
