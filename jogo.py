87b905676f35ed09200882dce216dbde4cb36095
LauraPerim <laurabatmanp@gmail.com>
2020-05-18 17:07:52 -0300

    alt

--- jogo.py
+++ jogo.py
@@ -1,15 +1,45 @@
 """
 Insper
-Authors: Diogo Nobre de Araujo Cintra, Luís Eduardo Satou Ferreira Pinheiro and Laura Batman Perim
-Graduating in Mechanical Engineering, Mechatronical Engineering and Computer Engineering
-Email: diogonac@al.insper.edu.br
-Email: luisesfp@al.insper.edu.br
+Authors: Pietro, Gianluca, and Laura Batman Perim
+Email: 
+Email:
 Email: laurabp@al.insper.edu.br
 """
 
-import pygame as pg
+import pygame 
 from os import path
 import random
 from os import path
 
-bg = pygame.image.load("C:\Users\laura\Documents\pygame2")
+
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('Hello World!')
+
+# ----- Inicia estruturas de dados
+game = True
+bg = pygame.image.load("imagem_fundo.png")
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(bg, (10, 10))
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+
+
+
