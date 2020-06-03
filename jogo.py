2d17707dd1f6c6f4cbf449940f336ebd430440a9
LauraPerim <laurabatmanp@gmail.com>
2020-06-03 18:13:21 -0300

    alt

--- jogo.py
+++ jogo.py
@@ -20,8 +20,8 @@ from os import path
 BLACK = (0, 0, 0)
 
 
-class Player(pygame.sprite.Sprite):
-     def __init__ (self):
+class Player(pygame.sprite.Sprite): # X
+     def __init__ (self,center):
          pygame.sprite.Sprite.__init__(self)
          img_x = pygame.image.load( "img_x.png")
          self.image=img_x
@@ -31,13 +31,12 @@ class Player(pygame.sprite.Sprite):
         # Detalhes sobre o posicionamento.
          self.rect = self.image.get_rect()
         
-        # Sorteia um lugar inicial em x
-         self.rect.x = 100
-        # Sorteia um lugar inicial em y
-         self.rect.y =100 
+        #  lugar x
+         self.rect.center = center
+
          
-class circulo(pygame.sprite.Sprite):
-     def __init__ (self):
+class circulo(pygame.sprite.Sprite): #0
+     def __init__ (self,center):
          pygame.sprite.Sprite.__init__(self)
          img_0 = pygame.image.load( "circulo_0.png")
          self.image=img_0
@@ -47,17 +46,12 @@ class circulo(pygame.sprite.Sprite):
         # Detalhes sobre o posicionamento.
          self.rect = self.image.get_rect()
         
-        # Sorteia um lugar inicial em x
-         self.rect.x = 200
         # Sorteia um lugar inicial em y
-         self.rect.y =150
-'''
-x,y=pygame.mouse.get_pos()
-x-=mouse_cursor.get_width()/2
-y-=mouse_cursor.get_height()/2
-screen.blit(mouse_cursor,(x,y))
-       
- '''       
+         self.rect.center=center
+         
+         
+         
+
          
 
 pygame.init()
@@ -74,28 +68,140 @@ bg = pygame.image.load("imagem_fundo.png")
 bg=pygame.transform.scale(bg,(500,400))
 
 
-
-player=Player()
 mobs = pygame.sprite.Group()
-mobs.add(player)
 
-Circulo=circulo()
+#dfrnvjnljfnj
+
+
+ret1=pygame.Rect(0,0,187,117)
+ret2=pygame.Rect(0,123,140,130)
+ret3=pygame.Rect(0,265,130,100)
+ret4=pygame.Rect(200,0,130,100)
+ret5=pygame.Rect(200,123,128,132)
+ret6=pygame.Rect(200,265,130,100)
+ret7=pygame.Rect(341,0,187,117)
+ret8=pygame.Rect(341,123,140,130)
+ret9=pygame.Rect(340,265,130,100)
+                 
+Circulo=circulo(ret1.center)
 cir=pygame.sprite.Group()
 cir.add(Circulo)
+
+
+
+font = pygame.font.SysFont(None, 48)
+text = font.render("teste", True, (0, 0, 255))
+
+
+jogX=True
 # ===== Loop principal =====
 while game:
+    
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            if ret1.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret1.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret1.center)
+                    jogX=True
+                mobs.add(player)
+            if ret2.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret2.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret2.center)
+                    jogX=True
+                mobs.add(player)
+            if ret3.collidepoint(event.pos): 
+                if jogX:
+                    player=Player(ret3.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret3.center)
+                    jogX=True
+                mobs.add(player)
+            if ret4.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret4.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret4.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+            
+            if ret5.collidepoint(event.pos): 
+                if jogX:
+                    player=Player(ret5.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret5.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+       
+            if ret6.collidepoint(event.pos): 
+                if jogX:
+                    player=Player(ret6.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret6.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+             
+            if ret7.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret7.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret7.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+             
+            if ret8.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret8.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret8.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+                
+            if ret9.collidepoint(event.pos):
+                if jogX:
+                    player=Player(ret9.center)
+                    jogX=False
+                else: 
+                    player=circulo(ret9.center)
+                    jogX=True
+                mobs.add(player)#SE COLIDIR COM RET 2
+            
+                
+                
+                
+                
+                
+            print (event.pos)
+       
+            
 
     # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.fill((255, 255, 255))  
     window.blit(bg, (0, 0))
-
-    mobs.draw(window)
-    cir.draw(window)
+    if jogX== True:
+        text = font.render("X", True, (0, 0, 255))
+    else:
+        text = font.render("0", True, (0, 0, 255))
+    window.blit(text,(0,0))
+  # pygame.draw.rect(window,(255,0,0),ret9)retangulos
+   
+    mobs.draw(window)  #X
+    cir.draw(window)  #0
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
