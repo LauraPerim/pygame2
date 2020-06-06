4aaf03ee7850dab0c522cebe102ee8ed697af828
Felipe Seixas <pietroas@al.insper.edu.br>
2020-06-06 11:56:14 -0300

    primeira tentativa do timer

---
+++ timer.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 11:41:13 2020
+
+@author: felip
+"""
+
+import os # For screen clear command
+import time # For timer
+
+minute = int(input('Enter any amount of minutes you want -+==> '))
+second = int(input('Enter any amount of seconds you want -+==> '))
+time =  minute*3600 + second*60
+print('{}:{}'.format(minute,second))
+while time > 0:
+   os.system("CLS") # Replace "{}" as "CLS" for windows or "CLEAR" for other.
+   time = time - 1
+   seconds = (time // 60) % 60
+   minutes = (time // 3600)
+   print('Time Left -+==> ',minutes,':',seconds,)
+if time == 0:
+   os.system("CLS") # Replace "{}" as "CLS" for windows or "CLEAR" for other. 
+   print('Time Is Over!')
