d9ec2a04897b348f58a5d07750d217a7a7e53c20
LauraPerim <laurabatmanp@gmail.com>
2020-06-08 10:57:58 -0300

    alt

--- jogo.py
+++ jogo.py
@@ -1,28 +1,26 @@
 """
 Insper
-<<<<<<< HEAD
 Authors: Gianluca Lazzaris Giudici, Laura Batman Perim & Pietro Abe Seixas
 Graduating in Computer Engineering
 Email: gianlucalg@al.insper.edu.br
 Email: laurabp@al.insper.edu.br
 Email: pietroas@al.insper.edu.br
 """
-
+#importando as classes necessarias
 import pygame 
 from os import path
 import random
-from os import path
 
 BLACK = (0, 0, 0)
 
-
+#criando a classe do x
 class Player(pygame.sprite.Sprite): # X
      def __init__ (self,center):
          pygame.sprite.Sprite.__init__(self)
          img_x = pygame.image.load( "img_x.png")
          self.image=img_x
          self.image = pygame.transform.scale(img_x, (140, 140))
-         #self.image.set_colorkey(BLACK)
+       
         
         # Detalhes sobre o posicionamento.
          self.rect = self.image.get_rect()
@@ -30,61 +28,44 @@ class Player(pygame.sprite.Sprite): # X
         #  lugar x
          self.rect.center = center
 
-         
+#criando a classe do circulo         
 class circulo(pygame.sprite.Sprite): #0
      def __init__ (self,center):
          pygame.sprite.Sprite.__init__(self)
          img_0 = pygame.image.load( "circulo_0.png")
          self.image=img_0
          self.image = pygame.transform.scale(img_0, (120, 90))
-         #self.image.set_colorkey(BLACK)
+     
         
         # Detalhes sobre o posicionamento.
          self.rect = self.image.get_rect()
         
         # Sorteia um lugar inicial em y
          self.rect.center=center
-'''       
-def verifica_vencedor(coordenadas):
-
-    return((ret[1] ==  ret[2] == ret[3])or
-          (ret[1] ==  ret[5] ==ret[9])or
-          (ret[7] ==  ret[5] ==ret[3])or
-          (ret[3] ==  ret[6] ==ret[9])or
-          (ret[2] ==  ret[5] ==ret[8])or
-          (ret[1] ==  ret[4] ==ret[7])or
-          (ret[4] ==  ret[5] ==ret[6])or
-          (ret[7] ==  ret[8] ==ret[9]))
-'''
-         
-
-        
-         
-         
+   
          
 
          
-
+#iniciando pygame
 pygame.init()
 
-WIDTH=500
 
-# ----- Gera tela principal
-window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption('Hello World!')
+# Gera tela principal
+window = pygame.display.set_mode((500, 400)) #altura e largura
+pygame.display.set_caption('jogo da velha')
 
 # ----- Inicia estruturas de dados
-game = True
-bg = pygame.image.load("imagem_fundo.png")
-bg=pygame.transform.scale(bg,(500,400))
+game = True #continuar o jogo enquanto o pygame for True
+bg = pygame.image.load("imagem_fundo.png") #imagem de fundo
+bg=pygame.transform.scale(bg,(500,400)) #posicionamento imagem de fundo
 
 
-mobs = pygame.sprite.Group()
+mobs = pygame.sprite.Group() #classe dos X
 
 
 
-
-ret1=pygame.Rect(40,0,187,117)
+#fazendo retangulos para definir o jogo
+ret1=pygame.Rect(40,0,180,117)
 ret2=pygame.Rect(40,123,150,130)
 ret3=pygame.Rect(40,265,180,100)
 ret4=pygame.Rect(200,0,130,117)
@@ -93,18 +74,20 @@ ret6=pygame.Rect(200,265,130,100)
 ret7=pygame.Rect(341,0,120,100)
 ret8=pygame.Rect(341,123,140,130)
 ret9=pygame.Rect(340,265,130,100)
+
+
                  
-Circulo=circulo(ret1.center)
-cir=pygame.sprite.Group()
-cir.add(Circulo)
+
+cir=pygame.sprite.Group() #definindo grupo circulos
 
 
 
-font = pygame.font.SysFont(None, 48)
+
+font = pygame.font.SysFont(None, 48) #texto
 text = font.render("teste", True, (0, 0, 255))
 
 
-jogX=True
+jogX=True  #enquanto o jogador for True
 # ===== Loop principal =====
 while game:
     
@@ -191,15 +174,36 @@ while game:
                     player=circulo(ret9.center)
                     jogX=True
                 mobs.add(player)#SE COLIDIR COM RET 2
+           
+matriz = [ ["", "", ""], ["", "", ""], ["", "", ""]]
+ 
+matriz[1][1] = "X"
+matriz[1][2] = "O"
+matriz[0][1] = "X"
+matriz[2][1] = "X"
+ 
+#print(matriz)
+
+for i in range(3):
+ if(matriz[i][0] == "X" and matriz[i][1] == "X" and matriz[i][2] == "X"):
+     print("GANHOU em linha {}".format(i))
+for i in range(3):
+ if(matriz[0][1] == "X" and matriz[1][i] == "X" and matriz[2][i] == "X"):
+     print("GANHOU em coluna{}".format(i))
+if(matriz[0][0] == "X" and matriz[1][1] == "X" and matriz[2][2] == "X"):
+    print("GANHOU em diag 1")
+if(matriz[0][2] == "X" and matriz[1][1] == "X" and matriz[2][0] == "X"):
+    print("GANHOU em diag 2")
+
+             
             
-                
-                
-                
-                
-                
-            print (event.pos)
+                    
        
-            
+                        
+  
+                
+               # print (event.pos)
+  
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  
@@ -210,6 +214,7 @@ while game:
         text = font.render("0", True, (0, 0, 255))
     window.blit(text,(0,0))
     #pygame.draw.rect(window,(255,0,0),ret3)  #desenha os retangulos
+
    
     mobs.draw(window)  #X
     cir.draw(window)  #0
